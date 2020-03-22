c4770a0218a9506a9f7c7ded6d0bc5f3ecbb06c2
fengzhizi715 <fengzhizi715@126.com>
2020-03-22 11:34:17 +0800

    update Response

--- core/src/main/java/com/safframework/androidserver/core/http/Response.kt
+++ core/src/main/java/com/safframework/androidserver/core/http/Response.kt
@@ -1,5 +1,8 @@
 package com.safframework.androidserver.core.http
 
+import io.netty.handler.codec.http.HttpResponseStatus
+import io.netty.util.AsciiString
+
 
 /**
  *
@@ -9,4 +12,19 @@ package com.safframework.androidserver.core.http
  * @date: 2020-03-21 13:09
  * @version: V1.0 <描述当前版本功能>
  */
-interface Response
+interface Response {
+
+    fun setStatus(status: HttpResponseStatus): Response
+
+    fun setBodyJson(serializeToJson: Any): Response
+
+    fun setBodyHtml(html: String): Response
+
+    fun setBodyData(contentType: String, data: ByteArray): Response
+
+    fun setBodyText(text: String): Response
+
+    fun addHeader(key: CharSequence, value: CharSequence): Response
+
+    fun addHeader(key: AsciiString, value: AsciiString): Response
+}
