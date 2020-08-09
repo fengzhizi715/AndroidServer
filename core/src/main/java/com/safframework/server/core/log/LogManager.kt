10e4f498ad579164e521e5d9a9624b241006ede0
fengzhizi715 <fengzhizi715@126.com>
2020-08-09 12:47:05 +0800

    update build.gradle

--- core/src/main/java/com/safframework/server/core/log/LogManager.kt
+++ core/src/main/java/com/safframework/server/core/log/LogManager.kt
@@ -1,12 +1,12 @@
 package com.safframework.server.core.log
 
 /**
- * AndroidServer 内部使用的日志操作
+ *
  * @FileName:
  *          com.safframework.server.core.log.LogManager
  * @author: Tony Shen
  * @date: 2020-03-24 13:28
- * @version: V1.0 <描述当前版本功能>
+ * @version: V1.0 AndroidServer 内部使用的日志操作
  */
 object LogManager {
 
